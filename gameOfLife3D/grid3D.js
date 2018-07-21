9f4164522d53440f7b5930857af5c4aec4197815
cocokhaern <corentin.khaern@gmail.com>
2018-07-21 18:17:24 +0200

    Initialisation du git, version 2D fonctionnel, version 3D a implementer

---
+++ gameOfLife3D/grid3D.js
@@ -0,0 +1,94 @@
+class Grid {
+
+    constructor(numX, numY, cellSize) {
+        this.numX = numX;
+        this.numY = numY;
+        this.cellSize = cellSize;
+        this.grid = [];
+    }
+
+    initialize() {
+        for (let x = 0; x < this.numX; x++) {
+            this.grid[x] = [];
+            for (let y = 0; y < this.numY; y++) {
+                this.grid[x][y] = new Cell(x, y, false);
+            }
+        }
+        this.grid[15][15].state = true;
+        this.grid[15][16].state = true;
+        this.grid[16][15].state = true;
+        this.grid[15][14].state = true;
+        this.grid[14][15].state = true;
+        this.grid[18][18].state = true;
+        this.grid[18][16].state = true;
+        this.grid[18][15].state = true;
+        this.grid[18][14].state = true;
+        this.grid[14][18].state = true;
+        this.grid[15][18].state = true;
+        this.grid[15][18].state = true;
+        this.grid[16][18].state = true;
+        this.grid[17][17].state = true;
+        this.grid[18][19].state = true;
+        this.grid[13][13].state = true;
+        this.grid[13][18].state = true;
+        this.grid[13][15].state = true;
+        this.grid[12][13].state = true;
+        this.grid[18][20].state = true;
+        this.grid[25][25].state = true;
+        this.grid[25][26].state = true;
+        this.grid[26][25].state = true;
+        this.grid[25][24].state = true;
+        this.grid[24][25].state = true;
+        this.grid[28][28].state = true;
+        this.grid[28][26].state = true;
+        this.grid[28][25].state = true;
+        this.grid[28][24].state = true;
+        this.grid[24][28].state = true;
+        this.grid[25][28].state = true;
+        this.grid[25][28].state = true;
+        this.grid[26][28].state = true;
+        this.grid[27][27].state = true;
+        this.grid[28][29].state = true;
+        this.grid[23][23].state = true;
+        this.grid[23][28].state = true;
+        this.grid[23][25].state = true;
+        this.grid[22][23].state = true;
+        this.grid[28][20].state = true;
+    }
+
+    prepareEvolution() {
+        this.grid.forEach(ligne => ligne.forEach(cell => cell.evolveCell(this.howManyNeighbours(cell.x, cell.y))));
+    }
+
+    commitEvolution() {
+        this.grid.forEach(ligne => ligne.forEach(cell => cell.commit()));
+    }
+
+    howManyNeighbours(nX, nY) {
+        let nbVoisins = 0;
+        for (var x = (nX - 1); x <= (nX + 1); x++) {
+            for (var y = (nY - 1); y <= (nY + 1); y++) {
+                if (x >= 0 && y >= 0 && x < this.numX && y < this.numY) {
+                    if (!(x == nX && y == nY) && this.grid[x][y].state == true) {
+                        nbVoisins = nbVoisins + 1;
+                    }
+                }
+            }
+        }
+        return nbVoisins;
+    }
+
+    drawGrid() {
+        this.grid.forEach(ligne => ligne.forEach(cell => cell.drawCell(this.cellSize)));
+    }
+
+    drawFutureGrid() {
+        this.grid.forEach(ligne => ligne.forEach(cell => cell.drawFutureCell(this.cellSize)));
+    }
+
+    readClick() {
+        let cellX = ceil(mouseX / this.cellSize);
+        let cellY = ceil(mouseY / this.cellSize);
+        this.grid[cellX-1][cellY-1].switchCell();
+    }
+}
---
+++ gameOfLife3D/sketch3D.js
@@ -0,0 +1,88 @@
+var myGrid;
+
+var framecompt = 0;
+var running = false;
+var count = 0;
+
+var textPlay = "PAUSE";
+
+// Parametres
+var globalSize = 90;
+var slowSize = 2;
+var withInter = true;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(60);
+  background(255);
+  noSmooth();
+  noStroke();
+  myGrid = new Grid(globalSize, globalSize, height / globalSize);
+  myGrid.initialize();
+  myGrid.drawGrid();
+}
+
+function draw() {
+
+  displayInfos();
+  let fpsControl = frameCount % slowSize;
+  if (running && fpsControl == 0) {
+    if (withInter) {
+      runSimWithInter();
+    } else {
+      runSimNoInter();
+    }
+  }
+  
+}
+
+function displayInfos(){
+  fill(100);
+  rect(height, 0, width-height, height);
+  fill(255);
+  textSize(height/12);
+  text(textPlay, height+40, (height/10));
+  text("Iteration : " + floor(count/2), height+40, 2*(height/10));
+  
+
+}
+
+
+function runSimWithInter() {
+  let pair = count % 2;
+  if (pair == 0) {
+    myGrid.drawGrid();
+    myGrid.prepareEvolution();
+    count = count + 1;
+  } else {
+    myGrid.drawFutureGrid();
+    myGrid.commitEvolution();
+    count = count + 1;
+  }
+}
+
+function runSimNoInter() {
+  myGrid.drawGrid();
+  myGrid.prepareEvolution();
+  myGrid.commitEvolution();
+  count = count + 2;
+}
+
+
+function keyPressed() {
+  if (keyCode === ENTER && !running) {
+    running = true;
+    textPlay = "PLAY";
+  } else if (keyCode === ENTER && running) {
+    running = false;
+    textPlay = "PAUSE";
+   }
+}
+
+function mouseClicked() {
+  myGrid.readClick();
+  myGrid.drawGrid();
+
+}
+
+
